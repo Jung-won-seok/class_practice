2b6dcc03986fc6691db5485edda65d9e0d673977
Jung-won-seok <ukksj0621@naver.com>
2024-07-03 21:54:43 +0900

    가변길이 인자

---
+++ src/main/java/length.java
@@ -0,0 +1,14 @@
+public class length {
+    static int addAll(int...k){
+        int result = 0;
+        for(int i : k){
+            result += i;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println(addAll(1, 2));
+        System.out.println(addAll(1, 2, 3));
+        System.out.println(addAll(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+    }
+}
