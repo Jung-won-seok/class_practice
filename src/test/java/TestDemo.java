6cca6ba44d49b94cffb1fd3caab05ddf06276e00
Jung-won-seok <ukksj0621@naver.com>
2024-07-15 23:15:22 +0900

    logging

---
+++ src/test/java/TestDemo.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class TestDemo {
+    @Test
+    void succeedingTest(){
+        assertTrue(3 % 2 == 0);
+    }
+    @RepeatedTest(10)
+    void failingTest(RepetitionInfo repetitionInfo){
+        if (repetitionInfo.getCurrentRepetition() == 3) {
+            fail("a failing test");
+        }
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4})
+    void testParameterizedTest(int n){
+        if(n==3)
+            fail("a failing test");
+    }
+}
