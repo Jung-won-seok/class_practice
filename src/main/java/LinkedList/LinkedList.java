202345174fef34a3841807de1374dffefac66352
Jung-won-seok <ukksj0621@naver.com>
2024-07-21 22:50:48 +0900

    LinkedList

--- src/main/java/LinkedList/LinkedList.java
+++ src/main/java/LinkedList/LinkedList.java
@@ -1,4 +1,29 @@
 package LinkedList;
 
 public class LinkedList {
+    private ListNode head;
+
+    public LinkedList() {
+        head = null;
+    }
+}
+
+class ListNode {
+    private String data;
+    public ListNode link;
+
+    public ListNode() {
+        this.data = null;
+        this.link = null;
+    }
+
+    public ListNode(String data) {
+        this.data = data;
+        this.link = null;
+    }
+
+    public ListNode(String data, ListNode link) {
+        this.data = data;
+        this.link = link;
+    }
 }
