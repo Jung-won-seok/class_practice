ac97a36e01ef59dc1150b461378dca9e37aed2f1
Jung-won-seok <ukksj0621@naver.com>
2024-07-31 23:53:34 +0900

    curl_check_args

---
+++ src/main/java/curl/check_args.java
@@ -0,0 +1,11 @@
+package curl;
+
+import java.util.Scanner;
+
+public class check_args {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+    }
+}
