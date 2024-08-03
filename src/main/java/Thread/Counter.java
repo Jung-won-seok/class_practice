317889bb42298d82e571f7a8c711372a56198421
Jung-won-seok <ukksj0621@naver.com>
2024-08-03 23:36:49 +0900

    thread

--- src/main/java/Thread/Counter.java
+++ src/main/java/Thread/Counter.java
@@ -24,8 +24,8 @@ public class Counter {
         }
     }
     public static void main(String[] args) {
-        Counter counter1 = new Counter("counter", 10);
-        Counter counter2 = new Counter("counter", 10);
+        Counter counter1 = new Counter("counter_1", 10);
+        Counter counter2 = new Counter("counter_2", 10);
         counter1.run();
         counter2.run();
     }
